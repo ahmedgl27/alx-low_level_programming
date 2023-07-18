cf218bc2ec36eeb70379d03b8b23c5d3e5c07a9d
ahmed hiti gueli <ahmedhitig@gmail.com>
2023-07-17 17:47:13 -0700

    last digit script added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the lowercase alphabet followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter;
+
+    /* Loop to print the lowercase alphabet */
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+    }
+
+    /* Print a new line after printing the alphabet */
+    putchar('\n');
+
+    return 0;
+}
