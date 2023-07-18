cd9da1a132093fc4c0ee8944ca212536d9d0db02
ahmed hiti gueli <ahmedhitig@gmail.com>
2023-07-17 17:58:25 -0700

    print numbers script added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints all single-digit numbers of base 10
+ *        starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int digit;
+
+    /* Loop to print all single-digit numbers */
+    for (digit = 0; digit < 10; digit++)
+    {
+        printf("%d", digit);
+    }
+
+    /* Print a new line after printing all the single-digit numbers */
+    printf("\n");
+
+    return 0;
+}
