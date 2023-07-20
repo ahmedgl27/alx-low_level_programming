1b4ef1d9e54fda2eb396edafc99c7f68670ba864
ahmed hiti gueli <ahmedhitig@gmail.com>
2023-07-20 05:04:11 -0700

    isupper program added

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-// Function prototype for _putchar
+/* Function prototype for _putchar */
 int _putchar(char c);
 
-// Function prototypes for other functions
+/* Function prototypes for other functions */
 int _isupper(int c);
-// Add more function prototypes here for your other functions
+/* Add more function prototypes here for your other functions */
 
 #endif /* MAIN_H */
