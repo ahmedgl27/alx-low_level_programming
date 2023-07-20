ff14efde2f7e9675717694f7dd1a47657ab1070a
ahmed hiti gueli <ahmedhitig@gmail.com>
2023-07-20 05:29:29 -0700

    main.h updated

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,7 @@ int _putchar(char c);
 /* isupper prototype */
 int _isupper(int c);
 /* isdigit function prototype */
-int _isdigit(int c); 
+int _isdigit(int c);
+/* function that multiplies two integers prototype */
+int mul(int a, int b);
 #endif /* MAIN_H */
